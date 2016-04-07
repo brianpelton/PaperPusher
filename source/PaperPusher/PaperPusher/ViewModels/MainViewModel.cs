455d1bdb9821b8cbb40259939277ca6e55ca53b2
brianpelton <brian.pelton@gmail.com>
2016-04-06 22:00:13 -0700

    Rough out main view model and view.

--- source/PaperPusher/PaperPusher/ViewModels/MainViewModel.cs
+++ source/PaperPusher/PaperPusher/ViewModels/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Caliburn.Micro;
+﻿using System.ComponentModel;
+using System.IO;
+using System.Windows.Media.Imaging;
+using Caliburn.Micro;
 using PropertyChanged;
 
 namespace PaperPusher.ViewModels
@@ -6,13 +9,62 @@ namespace PaperPusher.ViewModels
     [ImplementPropertyChanged]
     public class MainViewModel : Screen
     {
+        #region [ Constructors ]
+
+        public MainViewModel()
+        {
+            CurrentDirectory = new DirectoryInfo("c:\\temp\\pdfs");
+            TargetRootDirectory = new DirectoryInfo("c:\\temp\\target");
+        }
+
+        #endregion
+
         #region [ Properties ]
 
-        public string Name { get; set; } = "Fred";
+        public DirectoryInfo CurrentDirectory { get; set; }
 
-        public void ChangeName()
+        public BindingList<FileInfo> CurrentFiles { get; protected set; }
+            = new BindingList<FileInfo>();
+
+        public BitmapSource PreviewImage { get; private set; }
+        public FileInfo SelectedCurrentFile { get; set; }
+
+        public BindingList<DirectoryInfo> TargetDirectories { get; protected set; }
+            = new BindingList<DirectoryInfo>();
+
+        public DirectoryInfo TargetRootDirectory { get; set; }
+
+        #endregion
+
+        #region [ Methods ]
+
+        private void OnCurrentDirectoryChanged()
         {
-            Name = "Tim";
+            CurrentFiles.Clear();
+
+            if (CurrentDirectory == null ||
+                !CurrentDirectory.Exists)
+                return;
+
+            foreach (var file in CurrentDirectory.GetFiles())
+                CurrentFiles.Add(file);
+        }
+
+        private void OnSelectedCurrentFileChanged()
+        {
+            //todo: generate pdf preview.
+        }
+
+        private void OnTargetRootDirectoryChanged()
+        {
+            TargetDirectories.Clear();
+
+            if (TargetRootDirectory == null ||
+                !TargetRootDirectory.Exists)
+                return;
+
+            foreach (var directory in TargetRootDirectory.GetDirectories())
+                TargetDirectories.Add(directory);
         }
 
         #endregion
