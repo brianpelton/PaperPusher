4ae596aca7a6690661f209672afbd5af3741b8cc
brianpelton <brian.pelton@gmail.com>
2016-04-06 21:27:12 -0700

    Add propertychanged.

--- source/PaperPusher/PaperPusher/ViewModels/MainViewModel.cs
+++ source/PaperPusher/PaperPusher/ViewModels/MainViewModel.cs
@@ -1,12 +1,19 @@
 ﻿using Caliburn.Micro;
+using PropertyChanged;
 
 namespace PaperPusher.ViewModels
 {
+    [ImplementPropertyChanged]
     public class MainViewModel : Screen
     {
         #region [ Properties ]
 
         public string Name { get; set; } = "Fred";
+
+        public void ChangeName()
+        {
+            Name = "Tim";
+        }
 
         #endregion
     }
