aee45eb94e6ca5ed1810ccefb98c82419d26b4c3
brianpelton <brian.pelton@gmail.com>
2016-04-06 21:39:00 -0700

    Add MahApps

--- source/PaperPusher/PaperPusher/Views/MainView.xaml.cs
+++ source/PaperPusher/PaperPusher/Views/MainView.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace PaperPusher.Views
+﻿namespace PaperPusher.Views
 {
-    /// <summary>
-    /// Interaction logic for MainView.xaml
-    /// </summary>
-    public partial class MainView : UserControl
+    public partial class MainView
     {
+        #region [ Constructors ]
+
         public MainView()
         {
             InitializeComponent();
         }
+
+        #endregion
     }
 }
