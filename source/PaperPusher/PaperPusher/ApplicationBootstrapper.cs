00a4fd2eb3513359b1719fb71a4715f9daafbeb1
brianpelton <brian.pelton@gmail.com>
2016-04-06 21:14:51 -0700

    Add Caliburn Micro

---
+++ source/PaperPusher/PaperPusher/ApplicationBootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using PaperPusher.ViewModels;
+
+namespace PaperPusher
+{
+    public class ApplicationBootstrapper : BootstrapperBase
+    {
+        public ApplicationBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            base.OnStartup(sender, e);
+
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ source/PaperPusher/PaperPusher/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Caliburn.Micro;
+
+namespace PaperPusher.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        #region [ Properties ]
+
+        public string Name { get; set; } = "Fred";
+
+        #endregion
+    }
+}
