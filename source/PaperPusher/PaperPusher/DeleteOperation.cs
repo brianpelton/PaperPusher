b18ec449df84e04221ffdcf2ec93805dbdd2a70b
brianpelton <brian.pelton@gmail.com>
2016-04-12 21:36:21 -0700

    Add delete operation.

---
+++ source/PaperPusher/PaperPusher/DeleteOperation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace PaperPusher
+{
+    public class DeleteOperation : IOperation
+    {
+        #region [ Constructors ]
+
+        public DeleteOperation(FileInfo originalFile)
+        {
+            OriginalFile = originalFile;
+            var filename = Path.Combine(Settings.TrashBinPath, originalFile.Name);
+            NewFile = new FileInfo(filename);
+        }
+
+        #endregion
+
+        #region [ Properties ]
+
+        public string Description => $"Delete '{OriginalFile.Name}'";
+        public FileInfo NewFile { get; }
+        public FileInfo OriginalFile { get; }
+
+        #endregion
+
+        #region [ Interface IOperation Members ]
+
+        public void Do()
+        {
+            OriginalFile.MoveTo(NewFile.FullName);
+        }
+
+        public void Undo()
+        {
+            NewFile.MoveTo(OriginalFile.FullName);
+        }
+
+        #endregion
+    }
+}
---
+++ source/PaperPusher/PaperPusher/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PaperPusher
+{
+    public static class Settings
+    {
+        #region [ Properties ]
+
+        /// <summary>
+        ///     Enable deletion of all files in the trash bin.
+        /// </summary>
+        public static bool DeleteTrashBinOnExit { get; set; }
+
+        /// <summary>
+        ///     Location to move deleted files. This location
+        ///     can be set to delete its content on exit.
+        /// </summary>
+        public static string TrashBinPath { get; set; } = "c:\\temp\\trash";
+
+        #endregion
+    }
+}
