cff12a99ba4b11caf90c6641c42c14a130a507bf
brianpelton <brian.pelton@gmail.com>
2016-04-25 18:55:30 -0700

    Tests for delete operation

---
+++ source/PaperPusher/PaperPusher.Core.Tests/Operations/DeleteOperationTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaperPusher.Core.Operations;
+using Shouldly;
+
+namespace PaperPusher.Core.Tests.Operations
+{
+    [TestClass]
+    public class DeleteOperationTests
+    {
+        #region [ Methods ]
+
+        [TestMethod]
+        public void Can_Do_Delete_Operation()
+        {
+            // arrange
+            var file = Path.GetTempFileName();
+            var operation = new DeleteOperation(new FileInfo(file));
+
+            // assume
+            File.Exists(file).ShouldBe(true);
+
+            // act
+            operation.Do();
+
+            // assert
+            File.Exists(file).ShouldBe(false);
+        }
+
+        [TestMethod]
+        public void Can_Undo_Delete_Operation()
+        {
+            // arrange
+            var file = Path.GetTempFileName();
+            var operation = new DeleteOperation(new FileInfo(file));
+
+            // assume
+            operation.Do();
+            File.Exists(file).ShouldBe(false);
+
+            // act
+            operation.Undo();
+
+            // assert
+            File.Exists(file).ShouldBe(true);
+        }
+
+        #endregion
+    }
+}
---
+++ source/PaperPusher/PaperPusher.Core.Tests/TestInit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaperPusher.Core.Tests
+{
+    [TestClass]
+    public class TestInit
+    {
+        #region [ Methods ]
+
+        [AssemblyInitialize]
+        public static void Initialize(TestContext context)
+        {
+            Settings.TrashFolderPath = "c:\\temp\\Trash";
+        }
+
+        #endregion
+    }
+}
