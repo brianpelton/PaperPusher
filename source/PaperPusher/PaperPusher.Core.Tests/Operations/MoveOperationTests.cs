a8bbc91668e290938494f7a4029e5cebf71a9506
brianpelton <brian.pelton@gmail.com>
2016-04-25 20:13:54 -0700

    Add tests for move operation.

--- source/PaperPusher/PaperPusher.Core.Tests/Operations/MoveOperationTests.cs
+++ source/PaperPusher/PaperPusher.Core.Tests/Operations/MoveOperationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PaperPusher.Core.Operations;
 using Shouldly;
@@ -7,43 +8,51 @@ using Shouldly;
 namespace PaperPusher.Core.Tests.Operations
 {
     [TestClass]
-    public class DeleteOperationTests
+    public class MoveOperationTests
     {
         #region [ Methods ]
 
         [TestMethod]
-        public void Can_Do_Delete_Operation()
+        public void Can_Do_Move_Operation()
         {
             // arrange
             var file = Path.GetTempFileName();
-            var operation = new DeleteOperation(new FileInfo(file));
+            var newFilename = Path.GetTempPath() + Guid.NewGuid() + ".tmp";
+            var operation = new MoveOperation(
+                new FileInfo(file), new FileInfo(newFilename));
 
             // assume
             File.Exists(file).ShouldBe(true);
+            File.Exists(newFilename).ShouldBe(false);
 
             // act
             operation.Do();
 
             // assert
             File.Exists(file).ShouldBe(false);
+            File.Exists(newFilename).ShouldBe(true);
         }
 
         [TestMethod]
-        public void Can_Undo_Delete_Operation()
+        public void Can_Undo_Move_Operation()
         {
             // arrange
             var file = Path.GetTempFileName();
-            var operation = new DeleteOperation(new FileInfo(file));
+            var newFilename = Path.GetTempPath() + Guid.NewGuid() + ".tmp";
+            var operation = new MoveOperation(
+                new FileInfo(file), new FileInfo(newFilename));
             operation.Do();
 
             // assume
             File.Exists(file).ShouldBe(false);
+            File.Exists(newFilename).ShouldBe(true);
 
             // act
             operation.Undo();
 
             // assert
             File.Exists(file).ShouldBe(true);
+            File.Exists(newFilename).ShouldBe(false);
         }
 
         #endregion
